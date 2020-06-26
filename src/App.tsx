4abde10becfed96e958308cda9b97d8b8ea61bb9
Neezar <rforlyffe@gmaicl.com>
2020-06-26 18:06:09 +0800

    test circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import * as d3s from "d3";
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const svgRef = useRef<any>();
+
+  useEffect(() => {
+    const svg = d3s.select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", (value) => value)
+      .attr("cx", (val) => val + 2)
+      .attr("cy", (val) => val + 2)
+      .attr("stroke", "red");
+  }, [data]);
+  const HandleTimer = () => {
+    setTimeout(() => {
+      setData(data.map((res) => res + 10));
+      console.log("TIME");
+    }, 500);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "80vh", backgroundColor: "purple" }}>
+      <svg ref={svgRef} style={{ height: "100%", width: "100%" }}></svg>
+      <br />
+      <button onClick={() => setData(data.map((res) => res + 5))}>
+        update
+      </button>
+
+      <button onClick={() => HandleTimer()}>add size after 1 sec</button>
     </div>
   );
 }
